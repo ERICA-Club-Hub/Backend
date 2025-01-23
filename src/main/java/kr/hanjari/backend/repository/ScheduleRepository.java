7e164625c5618cfc5040ffb19b3ca70c7a70698f
msk226 <kminseok5167@gmail.com>
2025-01-23 21:42:05 +0900

    :recycle: Refactor: 월 별 일정 조회 시, 월 순서대로 조회 되도록 수정 -> Schedule로 변경 - #9

--- src/main/java/kr/hanjari/backend/repository/ScheduleRepository.java
+++ src/main/java/kr/hanjari/backend/repository/ScheduleRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import kr.hanjari.backend.domain.Schedule;
 import kr.hanjari.backend.domain.key.ScheduleId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, ScheduleId> {
 
+    @Query("SELECT s FROM Schedule s WHERE s.id.clubId = :clubId ORDER BY s.id.month")
     List<Schedule> findAllByClubId(Long clubId);
 }
