a0cad55ffd0d8bfccb8da97b2cfc91b536dff850
msk226 <kminseok5167@gmail.com>
2025-01-27 23:51:12 +0900

    :sparkles: Feature: AccessToken 관련 서비스 로직 구현 - #11

--- src/main/java/kr/hanjari/backend/service/user/UserCommandService.java
+++ src/main/java/kr/hanjari/backend/service/user/UserCommandService.java
@@ -2,6 +2,7 @@ package kr.hanjari.backend.service.user;
 
 import static kr.hanjari.backend.web.dto.user.UserResponseDTO.*;
 
+import kr.hanjari.backend.web.dto.user.UserRequestDTO;
 import kr.hanjari.backend.web.dto.user.UserResponseDTO;
 import kr.hanjari.backend.web.dto.user.UserResponseDTO.UserLoginDTO;
 
@@ -11,10 +12,14 @@ public interface UserCommandService {
     UserCodeDTO createCode(String clubName);
 
     // 인증 코드를 통한 로그인
-
-    UserLoginDTO login(String code);
+    UserLoginDTO login(UserRequestDTO.UserLoginDTO code);
 
     // 로그아웃
-    void logout();
+    void logout(String authorizationHeader);
 
+    // 토큰이 블랙리스트에 있는지 확인
+    boolean isContainToken(String token);
+
+    // 토큰으로 동아리 이름 조회
+    String getClubNameFromToken(String token);
 }
--- src/main/java/kr/hanjari/backend/service/user/impl/UserCommandServiceImpl.java
+++ src/main/java/kr/hanjari/backend/service/user/impl/UserCommandServiceImpl.java
@@ -1,15 +1,20 @@
 package kr.hanjari.backend.service.user.impl;
 
 import java.security.SecureRandom;
+import java.util.concurrent.TimeUnit;
 import kr.hanjari.backend.domain.Club;
 import kr.hanjari.backend.payload.code.status.ErrorStatus;
 import kr.hanjari.backend.payload.exception.GeneralException;
 import kr.hanjari.backend.repository.ClubRepository;
+import kr.hanjari.backend.service.auth.JwtTokenProvider;
 import kr.hanjari.backend.service.user.UserCommandService;
+import kr.hanjari.backend.web.dto.user.UserRequestDTO;
 import kr.hanjari.backend.web.dto.user.UserResponseDTO.UserCodeDTO;
 import kr.hanjari.backend.web.dto.user.UserResponseDTO.UserLoginDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +25,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserCommandServiceImpl implements UserCommandService {
 
     private final ClubRepository clubRepository;
+    private final StringRedisTemplate redisTemplate;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String BLACKLIST_KEY = "blacklist";
 
     private static final String CHAR_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // 코드에 사용할 문자 집합
     private static final int CODE_LENGTH = 6; // 코드 길이
@@ -34,7 +43,7 @@ public class UserCommandServiceImpl implements UserCommandService {
 
         do {
             code = generateRandomCode();
-            isUnique = clubRepository.existsByCode(code);
+            isUnique = !clubRepository.existsByCode(code);
         } while (!isUnique);
 
         club.updateCode(code);
@@ -44,20 +53,39 @@ public class UserCommandServiceImpl implements UserCommandService {
     }
 
     @Override
-    public UserLoginDTO login(String code) {
-        Club club = clubRepository.findByCode(code).orElseThrow(
+    public UserLoginDTO login(UserRequestDTO.UserLoginDTO request) {
+        Club club = clubRepository.findByCode(request.getCode()).orElseThrow(
                 () -> new GeneralException(ErrorStatus._INVALID_CODE));
 
         // TODO: JWT 토큰 발급
+        String accessToken = jwtTokenProvider.createToken(club.getName());
 
-
-        return UserLoginDTO.of("accessToken", club.getName(), code);
+        return UserLoginDTO.of(accessToken, club.getName(), request.getCode());
     }
 
     @Override
-    public void logout() {
-        // TODO: 로그아웃 기능 구현
+    public void logout(String authorizationHeader) {
+        String token = authorizationHeader.substring(7); // "Bearer " 제거
+        if (jwtTokenProvider.isTokenExpired(token))  {
+            return; // 만료된 토큰은 블랙리스트에 추가하지 않음
+        }
+        if (isContainToken(token)) {
+            throw new GeneralException(ErrorStatus._TOKEN_ALREADY_LOGOUT);
+        }
+        // Redis 블랙리스트에 토큰 추가
+        redisTemplate.opsForSet().add(BLACKLIST_KEY, token);
+        // 예시로 만료시간 설정 (1일)
+        redisTemplate.expire(BLACKLIST_KEY, 1, TimeUnit.DAYS);
+    }
 
+    @Override
+    public boolean isContainToken(String token) {
+        return redisTemplate.opsForSet().isMember(BLACKLIST_KEY, token);
+    }
+
+    @Override
+    public String getClubNameFromToken(String token) {
+        return jwtTokenProvider.getClubNameFromToken(token);
     }
 
     private String generateRandomCode() {
