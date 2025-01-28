5c9177ba38ccec95d25d3884d8ce969aaeef5d9c
lheesoo1020 <lheesoo1020@gmail.com>
2025-01-29 03:39:32 +0900

    :memo: Docs: 자료실 관련 API 명세서 설명 추가 - #15

--- src/main/java/kr/hanjari/backend/web/controller/DocumentController.java
+++ src/main/java/kr/hanjari/backend/web/controller/DocumentController.java
@@ -24,10 +24,13 @@ public class DocumentController {
 
     @Operation(summary = "[자료실] 자료실 업로드", description = """
             ## 자료실에 파일을 업로드합니다.
-            ### RequestBody
+            ## Request
+            ### requestBody(json)
             - **title**: 제목
-            ### Multipart/form-data
-            - **files**: 업로드하려는 파일 리스트
+            ### files(multipart/form-data의 list)
+            - 업로드하려는 파일 리스트
+            ## Response
+            - 생성된 document의 ID
             """)
     @PostMapping(name = "/", consumes = {"application/json", "multipart/form-data"})
     public ApiResponse<Long> postNewDocument(@RequestPart CreateDocumentRequest requestBody,
@@ -39,6 +42,8 @@ public class DocumentController {
 
     @Operation(summary = "[자료실] 자료 전체 조회", description = """
             ## 자료실에 업로드된 파일을 전부 조회합니다.
+            ## Response
+            - **documentDTOList**: documentId, title의 list
             """)
     @GetMapping("/")
     public ApiResponse<GetAllDocumentsResponse> getAllDocuments() {
@@ -47,13 +52,15 @@ public class DocumentController {
         return ApiResponse.onSuccess(result);
     }
 
-    @Operation(summary = "[자료실] 자료 상세 조회", description = """
-            ## 자료 하나를 상세조회합니다.
-            ### PathVariable
-            - documentId: 조회를 희망하는 자료의 id
+    @Operation(summary = "[자료실] 자료의 파일 조회", description = """
+            ## 자료 하나의 파일을 조회합니다.
+            ## PathVariable
+            - **documentId**: 조회를 희망하는 document의 ID
+            ## Response
+            - **fileDTOList**: fileName, downloadUrl의 list
             """)
     @GetMapping("/{documentId}")
-    public ApiResponse<GetDocumentFilesResponse> getDocumentDetail(@PathVariable Long documentId) {
+    public ApiResponse<GetDocumentFilesResponse> getDocumentFiles(@PathVariable Long documentId) {
 
         GetDocumentFilesResponse result = documentService.getDocumentFiles(documentId);
         return ApiResponse.onSuccess(result);
@@ -61,24 +68,28 @@ public class DocumentController {
 
     @Operation(summary = "[자료실] 자료실 수정", description = """
             ## 자료 하나를 수정합니다.
-            ### RequestBody
-            - **title**: 제목
-            ### Multipart/form-data
-            - **files**: 업로드하려는 파일 리스트
+            ## PathVariable
+            - **documentId**: 수정을 희망하는 document의 ID
+            ##　Request
+            ### requestBody(json)
+            - **title**: 수정하려는 제목
+            - **removedFileIDList**: 수정 후 삭제되는 fileId의 list
+            ### files(Multipart/form-data의 list)
+            - 수정 후 추가로 업로드되는 파일의 list
             """)
     @PatchMapping("/{documentId}")
     public ApiResponse<Void> updateDocument(@PathVariable Long documentId,
-                               @RequestPart UpdateDocumentRequest request,
+                               @RequestPart UpdateDocumentRequest requestBody,
                                @RequestPart List<MultipartFile> files) {
 
-        documentService.updateDocument(documentId, request, files);
+        documentService.updateDocument(documentId, requestBody, files);
         return ApiResponse.onSuccess();
     }
 
     @Operation(summary = "[자료실] 자료 삭제", description = """
             ## 자료실에 업로드된 자료를 하나 삭제합니다.
-            ### PathVariable
-            - documentId: 삭제를 희망하는 자료의 id
+            ## PathVariable
+            - documentId: 삭제를 희망하는 document의 ID
             """)
     @DeleteMapping("/{documentId}")
     public ApiResponse<Void> deleteDocument(@PathVariable Long documentId) {
