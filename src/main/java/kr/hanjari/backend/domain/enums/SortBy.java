3c92d9299ff2112b7f553240f56afebe577e43cd
msk226 <kminseok5167@gmail.com>
2025-02-02 19:02:55 +0900

    :sparkles: Feature: 동아리 검색 API 구현 - #19

--- src/main/java/kr/hanjari/backend/domain/enums/SortBy.java
+++ src/main/java/kr/hanjari/backend/domain/enums/SortBy.java
@@ -1,7 +1,19 @@
 package kr.hanjari.backend.domain.enums;
 
-public enum SortBy {
+import lombok.Getter;
+import org.springframework.data.domain.Sort;
 
-    // 가나다, 카테고리, 모집기준
-    NAME_ASC, CATEGORY_ASC, RECRUITMENT_STATUS_ASC,
+@Getter
+public enum SortBy {
+    NAME_ASC(Sort.by(Sort.Direction.ASC, "name")),
+    NAME_DESC(Sort.by(Sort.Direction.DESC, "name")),
+    RECRUITMENT_STATUS_ASC(Sort.by(Sort.Direction.ASC, "recruitmentStatus")
+            .and(Sort.by(Sort.Direction.ASC, "name"))); // 같은 상태에서는 name 기준 오름차순 정렬
+
+    private final Sort sort;
+
+    SortBy(Sort sort) {
+        this.sort = sort;
+    }
+
 }
--- src/main/java/kr/hanjari/backend/web/dto/club/response/ClubSearchResponseDTO.java
+++ src/main/java/kr/hanjari/backend/web/dto/club/response/ClubSearchResponseDTO.java
@@ -1,9 +1,17 @@
 package kr.hanjari.backend.web.dto.club.response;
 
 import java.util.List;
+import kr.hanjari.backend.domain.Club;
+import org.springframework.data.domain.Page;
 
 public record ClubSearchResponseDTO(
         List<ClubResponseDTO> clubs,
         int totalElements
 ) {
+    public static ClubSearchResponseDTO of(Page<Club> clubs) {
+        return new ClubSearchResponseDTO(
+                clubs.map(ClubResponseDTO::of).getContent(),
+                (int) clubs.getTotalElements()
+        );
+    }
 }
