2c204a7604c8aa0812a42619749d1eda2898022b
lheesoo1020 <lheesoo1020@gmail.com>
2025-02-11 18:23:12 +0900

    :pencil2: Fix: 일부 엔드포인트 맨 뒤 / 제거

--- src/main/java/kr/hanjari/backend/web/controller/HealthCheckController.java
+++ src/main/java/kr/hanjari/backend/web/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package kr.hanjari.backend.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hanjari.backend.payload.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health-check")
 public class HealthCheckController {
 
-    @GetMapping("/")
+    @Tag(name = "서버 상태 확인")
+    @GetMapping("")
     public ApiResponse<String> healthCheck() {
         return ApiResponse.onSuccess("서버 켜져있어용");
     }
