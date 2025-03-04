5fc1ddcc14b6eac8eec72e34bbddd5e35eafaecc
lheesoo1020 <lheesoo1020@gmail.com>
2025-03-04 15:45:00 +0900

    chore: prod 설정파일 추가

--- src/main/java/kr/hanjari/backend/config/SwaggerConfig.java
+++ src/main/java/kr/hanjari/backend/config/SwaggerConfig.java
@@ -7,10 +7,8 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"local", "dev"})
 public class SwaggerConfig {
 
     @Bean
