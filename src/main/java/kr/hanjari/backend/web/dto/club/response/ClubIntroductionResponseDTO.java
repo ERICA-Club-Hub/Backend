5ce7f348d67c730179ee6ec24e8411e797f2c101
msk226 <kminseok5167@gmail.com>
2025-08-07 14:11:11 +0900

    refactor: 모집 대상은 공연 모집 관련 조회 응답 API에서 응답 하도록 DTO 구조 변경

--- src/main/java/kr/hanjari/backend/web/dto/club/response/ClubIntroductionResponseDTO.java
+++ src/main/java/kr/hanjari/backend/web/dto/club/response/ClubIntroductionResponseDTO.java
@@ -1,25 +1,19 @@
 package kr.hanjari.backend.web.dto.club.response;
 
-import kr.hanjari.backend.domain.Club;
 import kr.hanjari.backend.domain.Introduction;
 
 public record ClubIntroductionResponseDTO(
-        ClubResponseDTO club,
         String introduction,
-        String activity,
-        String recruitment
+        String activity
 ) {
-    public static ClubIntroductionResponseDTO of(Club club, Introduction introduction, String profileImageUrl) {
+    public static ClubIntroductionResponseDTO of(Introduction introduction) {
         if (introduction == null) {
             return new ClubIntroductionResponseDTO(
-                    ClubResponseDTO.of(club, profileImageUrl),
-                    null, null, null);
+                    null, null);
         }
         return new ClubIntroductionResponseDTO(
-                ClubResponseDTO.of(club, profileImageUrl),
                 introduction.getContent1(),
-                introduction.getContent2(),
-                introduction.getContent3()
+                introduction.getContent2()
         );
     }
 }
--- src/main/java/kr/hanjari/backend/web/dto/club/response/ClubRecruitmentResponseDTO.java
+++ src/main/java/kr/hanjari/backend/web/dto/club/response/ClubRecruitmentResponseDTO.java
@@ -4,15 +4,17 @@ import kr.hanjari.backend.domain.Recruitment;
 
 public record ClubRecruitmentResponseDTO(
         String due,
+        String target,
         String notice,
         String etc
 ) {
-    public static ClubRecruitmentResponseDTO of(Recruitment recruitment) {
+    public static ClubRecruitmentResponseDTO of(Recruitment recruitment, String target) {
         if (recruitment == null) {
-            return new ClubRecruitmentResponseDTO(null, null, null);
+            return new ClubRecruitmentResponseDTO(null, null, null, null);
         }
         return new ClubRecruitmentResponseDTO(
                 recruitment.getContent1(),
+                target,
                 recruitment.getContent2(),
                 recruitment.getContent3()
         );
