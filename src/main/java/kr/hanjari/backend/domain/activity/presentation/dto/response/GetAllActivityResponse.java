8eb94eda36c6242c033218f14ff7a5f32e260d79
lheesoo1020 <71459841+lheesoo1020@users.noreply.github.com>
2025-11-04 13:34:51 +0900

    [REFACTOR] 몇몇 도메인 refactoring (#132)
    * refactor: 패키지 구조 변경
    
    * refactor: request, response dto명 통일
    
    * refactor: announcement refactor
    
    * refactor: document domain
    
    * refactor: feedback
    
    * refactor: Announcement command response 추가
    
    * feat: WebUtil 추가
    
    * refactor: activity
    
    * 트랜잭션 범위 수정

--- src/main/java/kr/hanjari/backend/domain/activity/presentation/dto/response/GetAllActivityResponse.java
+++ src/main/java/kr/hanjari/backend/domain/activity/presentation/dto/response/GetAllActivityResponse.java
@@ -1,5 +1,7 @@
 package kr.hanjari.backend.domain.activity.presentation.dto.response;
 
+import kr.hanjari.backend.domain.activity.presentation.dto.ActivityThumbnailDTO;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import kr.hanjari.backend.domain.activity.presentation.dto.ActivityThumbnailDTO;
