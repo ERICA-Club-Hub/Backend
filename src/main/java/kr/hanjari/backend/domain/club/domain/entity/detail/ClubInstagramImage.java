3de18e0f00921b5fc75b1abf207f58d3c264239f
lheesoo1020 <lheesoo1020@gmail.com>
2026-01-02 15:56:35 +0900

    feat: selenium driver 설정 추가

--- src/main/java/kr/hanjari/backend/domain/club/domain/entity/detail/ClubInstagramImage.java
+++ src/main/java/kr/hanjari/backend/domain/club/domain/entity/detail/ClubInstagramImage.java
@@ -2,21 +2,25 @@ package kr.hanjari.backend.domain.club.domain.entity.detail;
 
 import jakarta.persistence.*;
 import kr.hanjari.backend.domain.club.domain.entity.Club;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "club_instagram_iamge")
+@Table(name = "club_instagram_image")
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class ClubInstagramImage {
+
+    public ClubInstagramImage(Long clubId) {
+        this.clubId = clubId;
+    }
 
     @Id
     @Column(name = "club_id")
     private Long clubId;
 
+    @Setter
     @MapsId(value = "clubId")
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "club_id")
