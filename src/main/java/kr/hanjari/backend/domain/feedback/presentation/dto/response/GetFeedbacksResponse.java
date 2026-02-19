16c67b1cd9507048d19f52206e50dc62f6599384
lheesoo1020 <71459841+lheesoo1020@users.noreply.github.com>
2026-02-20 01:35:03 +0900

    [FEATURE] 피드백 전체 조회 API 추가 (#166)
    * feat: 동아리 등록 요청 전체 조회 return field 변경
    
    * feat: 동아리 등록 요청 상세 조회 API 구현
    
    * feat: swagger tag 추가
    
    * feat: feedback 조회 api 추가

---
+++ src/main/java/kr/hanjari/backend/domain/feedback/presentation/dto/response/GetFeedbacksResponse.java
@@ -0,0 +1,16 @@
+package kr.hanjari.backend.domain.feedback.presentation.dto.response;
+
+import kr.hanjari.backend.domain.feedback.domain.entity.Feedback;
+import kr.hanjari.backend.domain.feedback.presentation.dto.FeedbackDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public record GetFeedbacksResponse(
+        int totalElements,
+        List<FeedbackDTO> feedbackDTOList
+) {
+    public static GetFeedbacksResponse of(Page<FeedbackDTO> feedbackPage) {
+        return new GetFeedbacksResponse(feedbackPage.getNumberOfElements(), feedbackPage.getContent());
+    }
+}
