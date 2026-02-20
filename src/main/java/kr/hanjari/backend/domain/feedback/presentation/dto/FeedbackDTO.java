345bddbab201bd8d95da5fe2c04f03e652737d76
lheesoo1020 <lheesoo1020@gmail.com>
2026-02-20 19:05:12 +0900

    feat: 피드백 조회 field 추가

--- src/main/java/kr/hanjari/backend/domain/feedback/presentation/dto/FeedbackDTO.java
+++ src/main/java/kr/hanjari/backend/domain/feedback/presentation/dto/FeedbackDTO.java
@@ -4,12 +4,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import kr.hanjari.backend.domain.feedback.domain.entity.Feedback;
 
+import java.time.LocalDateTime;
+
 @Schema(description = "DTO for feedback")
 public record FeedbackDTO(
         Long feedbackId,
         @Schema(description = "Feedback content", example = "Great service!", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotBlank(message = "Feedback content cannot be empty.")
-        String content
+        String content,
+        LocalDateTime dateTime
 ) {
     public Feedback toFeedback() {
         return Feedback.builder()
@@ -20,6 +23,7 @@ public record FeedbackDTO(
     public static FeedbackDTO fromFeedback(Feedback feedback) {
         return new FeedbackDTO(
                 feedback.getId(),
-                feedback.getContent());
+                feedback.getContent(),
+                feedback.getCreatedAt());
     }
 }
