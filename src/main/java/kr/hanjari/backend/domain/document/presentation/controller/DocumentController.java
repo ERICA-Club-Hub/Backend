aa544b80aad3a6988e410d00acda504fcdde7cb2
lheesoo1020 <lheesoo1020@gmail.com>
2026-04-30 00:29:46 +0900

    chore: mark activity, document, announcement controllers as deprecated

--- src/main/java/kr/hanjari/backend/domain/document/presentation/controller/DocumentController.java
+++ src/main/java/kr/hanjari/backend/domain/document/presentation/controller/DocumentController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Deprecated
 @RestController
 @RequestMapping("/api/documents")
 @RequiredArgsConstructor
